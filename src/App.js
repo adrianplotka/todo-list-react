3c13d2e45deca30cb32dc7e36136c3b94675575c
Adrian Płotka <adrianplotkaa@gmail.com>
2022-12-05 00:42:52 +0100

    create custom hooks "useTasks"

--- src/App.js
+++ src/App.js
@@ -4,48 +4,22 @@ import Buttons from "./Buttons";
 import Section from "./Section";
 import Header from "./Header";
 import Container from "./Container";
-import { useEffect, useState } from "react";
+import { useState } from "react";
+import { useTasks} from "./useTasks"
 
 function App() {
   const [hideDone, setHideDone] = useState(false);
-  const [tasks, setTasks] = useState(JSON.parse(localStorage.getItem("tasks")) || [])
-
-  useEffect(() => {
-    localStorage.setItem("tasks", JSON.stringify(tasks));
-  }, [tasks]);
-
   const toogleHideDone = () => {
     setHideDone(hideDone => !hideDone);
   };
 
-  const removeTask = (id) => {
-    setTasks(tasks => tasks.filter(task => task.id !== id));
-  };
-
-  const toggleTaskDone = (id) => {
-    setTasks(tasks => tasks.map(task => task.id === id ? 
-      { ...task, done: !task.done } :task
-     
-    ));
-  };
-
-  const setAllDone = () => {
-    setTasks(tasks => tasks.map(task => ({
-      ...task,
-      done: true,
-    })));
-  };
-
-  const addNewTask = (content) => {
-    setTasks(tasks => [...tasks,
-    {
-      content,
-      done: false,
-      id: tasks.length === 0 ? 1 : tasks[tasks.length - 1].id + 1,
-    },
-    ]);
-  };
-
+  const {
+    tasks,
+    removeTask,
+    toggleTaskDone,
+    setAllDone,
+    addNewTask,
+  } = useTasks();
  
 
   return (
