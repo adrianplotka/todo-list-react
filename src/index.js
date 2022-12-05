a516008fc76afb47ab1444cae0f1b7508a7e3006
Adrian Płotka <adrianplotkaa@gmail.com>
2022-12-05 01:53:20 +0100

    create GlobalStyle

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from 'styled-components';
-import './index.css';
+import { GlobalStyle } from "./GlobalStyle";
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { theme } from "./theme";
@@ -10,7 +10,8 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <App />
+      <GlobalStyle />
+        <App />
     </ThemeProvider>
   </React.StrictMode>
 );
